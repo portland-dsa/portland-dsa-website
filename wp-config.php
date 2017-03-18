3c78f86f98edeaad472e19f4c45fd65c06672984
Gina Giampaolo <gina@rumo.rs>
2017-03-17 23:36:21 -0700

    Update Wordpress to 4.7.3

--- wp-config.php
+++ wp-config.php
@@ -16,17 +16,17 @@
 
 // TODO: Add environment settings for development, staging, and production
 
-$root_url = 'http://path.to.website';
+// $root_url = 'portland-dsa.dev';
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'portland_dsa');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
