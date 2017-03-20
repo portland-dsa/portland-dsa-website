e97a1522dc25464436bff87ded6a0f39b730f82c
Gina Giampaolo <gina@rumo.rs>
2017-03-19 17:21:29 -0700

    Move document head to header.php

--- wp-content/themes/portland-dsa/views/pages/home.php
+++ wp-content/themes/portland-dsa/views/pages/home.php
@@ -1,10 +1,2 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Portland DSA</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head();?>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css ">
-</head>
 <body <?php body_class();?>">
+  homepage
--- wp-content/themes/portland-dsa/views/shared/header.php
+++ wp-content/themes/portland-dsa/views/shared/header.php
@@ -1,2 +1,14 @@
-<header>
-</header>
+<!DOCTYPE html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Portland DSA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head();?>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css ">
+  </head>
+
+  <header>
+    <h1>Democratic Socialists of America</h1>
+    <h1>Portland, Oregon</h1>
+  </header>
