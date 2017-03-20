0bd6ad136e66a0a77df57d5f13cc3b5f048b6a04
Gina Giampaolo <gina@rumo.rs>
2017-03-19 22:15:39 -0700

    Add & style homepage strips
    Hardcoded for now

--- wp-content/themes/portland-dsa/views/pages/home.php
+++ wp-content/themes/portland-dsa/views/pages/home.php
@@ -2,3 +2,28 @@
 <section class="callout">
   <p>We are a local chapter of the Democratic Socialists of America, the largest socialist organization in the US. We organize together for economic, racial, and social justice in Portland and nation-wide. <a>Learn more about what we stand for.</a></p>
 </section>
+<main class="main-content">
+  <article class="homepage-strip">
+    <div class="homepage-strip--image"></div>
+    <div class="homepage-strip--content">
+      <h1 class="homepage-strip--content-headline">What’s democratic socialism?</h1>
+      <p>It’s the idea that XYZ. Democratic socialists believe that a just economy and society should be run democratically, and that something something economics. Pro-woman, pro-immigrant, anti-racist. <a>Learn more about democratic socialism</a></p>
+    </div>
+  </article>
+
+  <article class="homepage-strip">
+    <div class="homepage-strip--image"></div>
+    <div class="homepage-strip--content">
+      <h1 class="homepage-strip--content-headline">Find upcoming events</h1>
+      <p>From chapter meetings and happy hours to demonstrations, actions, and community events, there’s something for everyone. And it’s almost all open to newcomers. <a>View our calendar of events</a></p>
+    </div>
+  </article>
+
+  <article class="homepage-strip">
+    <div class="homepage-strip--image"></div>
+    <div class="homepage-strip--content">
+      <h1 class="homepage-strip--content-headline">Organize with us</h1>
+      <p>Ready to join? We’ll help you become a national member, and get you on board locally. <a>Join us!</a></p>
+    </div>
+  </article>
+</main>
