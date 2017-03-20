391b4bf7794f4c08e5246f681ae9324a639c6221
Gina Giampaolo <gina@rumo.rs>
2017-03-19 22:14:54 -0700

    Add & style callout
    This should probably be a partial of some sort

--- wp-content/themes/portland-dsa/views/pages/home.php
+++ wp-content/themes/portland-dsa/views/pages/home.php
@@ -1,1 +1,4 @@
 <body <?php body_class();?>">
+<section class="callout">
+  <p>We are a local chapter of the Democratic Socialists of America, the largest socialist organization in the US. We organize together for economic, racial, and social justice in Portland and nation-wide. <a>Learn more about what we stand for.</a></p>
+</section>
