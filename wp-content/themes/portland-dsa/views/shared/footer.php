cb673ab095e156ec51f1a9dbb462b6301622bf4d
Gina Giampaolo <gina@rumo.rs>
2017-03-19 22:17:39 -0700

    Add & style footer

--- wp-content/themes/portland-dsa/views/shared/footer.php
+++ wp-content/themes/portland-dsa/views/shared/footer.php
@@ -1,4 +1,23 @@
   <footer>
+    <p>
+    Democratic Socialists of America<br>
+    Portland, Oregon Chapter<br>
+    <a>info@PortlandDSA.org</a>
+    </p>
+
+    <nav class="navigation-social">
+        <ul>
+          <li class="navigation-social--element-container">
+            <a href="https://twitter.com/portlanddsa" target="_blank" class="navigation-social--twitter social-icon"></a>
+          </li>
+          <li class="navigation-social--element-container">
+            <a href="https://www.facebook.com/groups/PortlandDSA/" target="_blank" class="navigation-social--facebook social-icon"></a>
+          </li>
+          <li class="navigation-social--element-container">
+            <a href="https://www.instagram.com/portlanddsa/" target="_blank" class="navigation-social--instagram social-icon"></a>
+          </li>
+        </ul>
+      </nav>
   </footer>
   </body>
 </html>
