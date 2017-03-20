0de65cd7d97901cd94f8b5949e259d221fd72d56
Gina Giampaolo <gina@rumo.rs>
2017-03-19 18:53:46 -0700

    Ignore compiled stylesheets

---
+++ wp-content/themes/portland-dsa/views/shared/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Portland DSA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head();?>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css ">
+  </head>
+
+  <header>
+    <h1>Democratic Socialists of America</h1>
+    <h1>Portland, Oregon</h1>
+    <ul>
+      <li>Who we are</li>
+      <li>What is democratic socialism</li>
+      <li>Calendar</li>
+      <li>Join us</li>
+    </ul/>
+  </header>
